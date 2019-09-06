18c42bc422bd1ec60eb15893f496e0cb60682a20
David Tingley <davidtingley2@gmail.com>
2019-09-06 10:15:52 -0400

    upload general tutorial

--- replay/tutorial.m
+++ replay/tutorial.m
@@ -1,13 +1,18 @@
-% initial parameters
+% a general script that creates place fields and 'replay' events, then shows 
+% the rank order correlation and radon integral for these events, compared to shuffled data.
+%
+% david tingley 2019
 
+
+%% initial parameters
 numCells = 100; % # of place fields for simulations
 
 
 
 %% first, let's make some place fields.
-offsets_rate = {round(sigmoid(1:100,50,.09) .* 100)... % Dupret style
-          1:100 ... % linearly spaced PFs
-          randi(100,1,100)}; % randomly spaced PFs
+offsets_rate = {round(sigmoid(1:100,50,.09) .* 100)... % Dupret style, reward/end over-representation
+          1:100 ...                                    % linearly spaced PFs
+          randi(100,1,100)};                           % randomly spaced PFs
 
 for o = 1:length(offsets_rate)
     for cell = 1:numCells
@@ -20,7 +25,7 @@ end
 
 %% ok, now let's make some ripple examples..
 offsets_rip = {round([(numCells/2-logspace(2,0.8,50)./2)+3 (logspace(0.8,2,50)./2)+numCells/2-3])... 
-           1:100};
+               1:100};
        
 for o =1:length(offsets_rip)
     for oo = 1:length(offsets_rate)
@@ -62,19 +67,25 @@ for o = 1:length(offsets_rip)
     for oo = 1:length(offsets_rate)
         subplot(conditions,4,cond*4-3)
         imagesc(rateMaps{oo})
+        xlabel('position')
+        ylabel('cell #')
         
         subplot(conditions,4,cond*4-2)
         imagesc(rippleEvent{o})
+        title('ripple template')
+        xlabel('time bin')
         
         subplot(conditions,4,cond*4-1)
         histogram(integral{o,oo},[0:.001:.05])
         hold on
         histogram(integral_shuffle{o,oo},[0:.001:.05])    
+        title('radon integral')
         
         subplot(conditions,4,cond*4)
         histogram(rankOrder{o,oo})
         hold on
         histogram(rankOrder_shuf{o,oo})
+        title('rank order correlation')
         
         cond = 1+cond;
     end
