f07e3b80e763ae49f9f2dde9aada8c0ceb879404
adrien <adrien.peyrache@gmail.com>
2019-12-18 14:59:25 -0500

    few edits

--- replay/compareBinSizes.m
+++ replay/compareBinSizes.m
@@ -3,8 +3,13 @@
 % these two neurons have different in-field firing rates. It
 % demonstrates that the choice of bin size can determine the decoded
 % position.
+
+% Copyright (C) 2019 Adrien Peyrache & David Tingley.
 %
-% david tingley 2019
+% This program is free software; you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation; either version 2 of the License, or
+% (at your option) any later version.
 
 
 %% first, let's make some place fields.
