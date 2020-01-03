f2f06fa5a2f204b76ecc364c8e2b806573debfc0
adrien <adrien.peyrache@gmail.com>
2020-01-03 13:21:48 -0500

    few changes

--- replay/replay_Bayesian.m
+++ replay/replay_Bayesian.m
@@ -30,7 +30,7 @@ function [replayScores] = replay_Bayesian(spikes,ripples,template,include)
 %
 % See bz_GetSpikes from the buzcode repo for help with the spikes/ripples
 %   data structures
-%
+
 % Copyright (C) 2019 Adrien Peyrache & David Tingley.
 %
 % This program is free software; you can redistribute it and/or modify
